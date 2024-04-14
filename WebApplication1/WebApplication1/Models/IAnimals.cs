5604c75030db7857a370404ad3a308b2e9032588
Mateusz Wiechetek <s22976@pjwstk.edu.pl>
2024-04-14 20:27:54 +0200

    Kod z lekcji

---
+++ WebApplication1/WebApplication1/Models/IAnimals.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Animals
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string Area { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Repositories/AnimalRepositories.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Repositories
+{
+    public class AnimalRepositories
+    {
+        SqlDataReader dr = com.ExecuteReader();
+        List<Animal> animals = new List<Animal>();
+
+    while (dr.Read())
+{
+        Animal a = new Animal();
+        a.Id = (int) dr["id"];
+        a.Name = (string) dr["name"];
+        a.Description = (string) dr["description"];
+        a.Area = (string) dr["Area"];
+        a.Category = (string) dr["category"];
+
+        animals.Add(a);
+}
+
+return animals;
+    }
+}
